a6c99a30dcfcfef8286c71d490dd7e25f8cfa557
M-lie <camelielaprise@hotmail.com>
2023-10-13 01:42:24 -0400

    erreur
    Revert "erreur"
    
    This reverts commit de05beb59f8e18a0fe2db8fc3d7f124151eeda79.
    
    erreur
    
    creations-pages-projets
    
    creations-pages-projets

--- script.js
+++ script.js
@@ -5,12 +5,28 @@ gsap.from('.bi-mouse', {
     y: '50%',
 });
 
-const swiper = new Swiper(".mySwiper", {
-    direction: "horizontal",
+const swiper1 = new Swiper(".mySwiper", {
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});
 
-    pagination: {
-      el: ".swiper-pagination",
-      draggable: true,
-      dragSize: "10px",
-    },
+const upper = document.getElementsByClassName('upper');
+const middle = document.getElementsByClassName('middle');
+const lower = document.getElementsByClassName('lower');
+
+const tl = new TimelineLite({paused: true, reversed: true});
+
+tl
+  .to(upper, 0.5, {attr: {d: "M8,2 L2,8"}, x: 1, ease:Power2.easeInOut}, 'start')
+  .to(middle, 0.5, {autoAlpha: 0}, 'start')
+  .to(lower, 0.5, {attr: {d: "M8,8 L2,2"}, x: 1, ease:Power2.easeInOut}, 'start');
+
+document.querySelector('.hamburger').addEventListener('click', function(){
+  tl.reversed() ? tl.play() : tl.reverse();
+})
+
+const swiper2 = new Swiper(".mySceneSwiper", {
+  direction: "horizontal",
 });
